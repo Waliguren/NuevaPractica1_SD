bdc929309806b9fd96050f2ea2ba2402a69f0617
Waliguren <91369888+Waliguren@users.noreply.github.com>
2026-05-21 10:49:20 +0200

    Arreglado 2 reset_redis.py

--- archivosCliente/reset_redis.py
+++ archivosCliente/reset_redis.py
@@ -2,30 +2,25 @@ import redis
 import sys
 import os
 
-# Cambia esto por la IP privada de tu máquina Redis
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_PASSWORD = "admin123"
 
 def resetear_redis():
     print(f"Conectando a Redis en {REDIS_HOST}...")
     try:
-        # Nos conectamos a Redis
         r = redis.Redis(host=REDIS_HOST, port=6379, password=REDIS_PASSWORD, decode_responses=True)
-        r.ping() # Comprobamos que la conexión funciona
+        r.ping() 
 
-        # 1. Resetear NO numeradas (Ponemos el contador a 20000)
         r.set('entradas_disponibles', 20000)
         print("✅ Entradas NO numeradas: Contador reseteado a 20.000")
 
-        # 2. Resetear NUMERADAS (Borramos el registro de asientos ocupados)
-        # Al hacer DEL del hash 'entradas_numeradas', liberamos todos los asien>
         r.delete('entradas_numeradas')
         print("✅ Entradas NUMERADAS: Todos los asientos han sido liberados")
 
         print("\n🚀 ¡Base de datos limpia y lista para el próximo benchmark!")
 
     except redis.ConnectionError:
-        print("❌ Error: No me puedo conectar a Redis. Revisa la IP y el Securi>
+        print("❌ Error: No me puedo conectar a Redis. Revisa la IP y el Security Group.")
     except Exception as e:
         print(f"❌ Error inesperado: {e}")
 
